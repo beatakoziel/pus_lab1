479097d01de5fc0853f860191d2ed5c33b2af6cc
Beata <beatainez.k@gmail.com>
2021-03-14 22:18:59 +0100

    deleted empty task 5 file

--- task5.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by beatinho on 14.03.2021.
-//
-
-void task5() {}
--- task5.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by beatinho on 14.03.2021.
-//
-
-#ifndef LAB1_TASK5_H
-#define LAB1_TASK5_H
-
-void task5();
-#endif //LAB1_TASK5_H
